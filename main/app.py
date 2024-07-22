e18dc0cff9e3e209c338a08ce619018008756ce5
Kappappa <minhquangtq2003@gmail.com>
2024-07-22 18:42:41 +0700

    Update

--- main/app.py
+++ main/app.py
@@ -1,17 +1,22 @@
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Response, File, UploadFile
+from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
-from main import *
 from fastapi.middleware.cors import CORSMiddleware
 import json
+import io
+import numpy as np
+from PIL import Image
+from main import scan_document, get_text
 import os
+import uuid
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust this according to your needs
+    allow_origins=["*"],  # Điều chỉnh cái này theo nhu cầu của bạn
     allow_credentials=True,
-    allow_methods=["*"],  # This allows all HTTP methods
+    allow_methods=["*"],  # Điều này cho phép tất cả các phương thức HTTP
     allow_headers=["*"],
 )
 
@@ -19,10 +24,33 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request,"ocr_endpoint": " http://127.0.0.1:8000/ocr"})
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/uploadfile/", response_class=HTMLResponse)
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = np.array(Image.open(io.BytesIO(contents)))
+    scanned_image = scan_document(image)
+    is_success, buffer = cv2.imencode(".jpg", scanned_image)
+    file_name = f"{uuid.uuid4()}.jpg"
+    file_path = f"scanned_images/{file_name}"
+
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    with open(file_path, "wb") as f:
+        f.write(buffer)
+
+    return {"download_url": f"/download/{file_name}"}
+
+@app.get("/download/{file_name}", response_class=HTMLResponse)
+async def download_file(file_name: str):
+    file_path = f"scanned_images/{file_name}"
+    return StreamingResponse(io.BytesIO(open(file_path, "rb").read()), media_type="image/jpeg")
+
 @app.post("/ocr")
-async def ocr(data:ORC_Check):
-    #rgb image pil image
-    image = scan_document(data.image)
-    text  = get_text(image)
-    return Response(json.dumps({"data":text}))
+async def ocr(data: UploadFile = File(...)):
+    contents = await data.read()
+    image = np.array(Image.open(io.BytesIO(contents)))
+    scanned_image = scan_document(image)
+    text = get_text(scanned_image)
+    return Response(json.dumps({"data": text}), media_type="application/json")
